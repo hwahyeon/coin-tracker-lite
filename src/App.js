51c9e8891c9b889693d1f2e078aba501a5985d06
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-19 20:46:41 +0900

    Removed todo.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import Todo from "./components/Todo";
 import Coin from "./components/Coin";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/reactjs_small/" element={<Home />} />
-                <Route path="/reactjs_small/todo" element={<Todo />} />
-                <Route path="/reactjs_small/coin" element={<Coin />} />
+                <Route path="/" element={<Coin />} />
             </Routes>
         </Router>
     );
